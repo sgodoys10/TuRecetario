7ac9a5cacaf80504eada1b63d4c8a6fbd9564b23
sgodoys10 <sgodoys10@gmail.com>
2024-07-17 20:24:48 -0700

    new changes

--- Recetario/src/App.jsx
+++ Recetario/src/App.jsx
@@ -32,32 +32,33 @@ function App() {
       description: "Descripción de la receta 3",
       buttonText: "Ver receta 3",
       link: "/comidas/3"
-    },
-    // Add more card data as needed
+    }
   ];
   return (
     <div className='App'> 
-    <Navbar />
-    <div className='card-container-2'>
-    {cardData.map((card, index) => (
-      <Card 
-        key={index}
-        imgSrc={card.imgSrc}
-        title={card.title}
-        description={card.description}
-        buttonText={card.buttonText}
-        link={card.link}
-        className='card-item'
-      />
-    ))}
+      <Navbar />
+      <div className='content'>
+      <Routes>
+        <Route path="/" element= {<Home/>}/>
+        <Route path="/comidas" element= {<Comidas/>}/>
+        <Route path="/bebidas" element= {<Bebidas/>}/>
+        <Route path="/postres" element= {<Postres/>}/>
+        <Route path="/chefs" element= {<Chefs/>}/>s
+      </Routes>
+      <div className='card-container-2'>
+        {cardData.map((card, index) => (
+          <Card 
+            key={index}
+            imgSrc={card.imgSrc}
+            title={card.title}
+            description={card.description}
+            buttonText={card.buttonText}
+            link={card.link}
+            className='card-item'
+          />
+        ))}
+      </div>
     </div> 
-    <Routes>
-      <Route path="/" element= {<Home/>}/>
-      <Route path="/comidas" element= {<Comidas/>}/>
-      <Route path="/bebidas" element= {<Bebidas/>}/>
-      <Route path="/postres" element= {<Postres/>}/>
-      <Route path="/chefs" element= {<Chefs/>}/>s
-    </Routes>
   </div>
   ); 
 }
